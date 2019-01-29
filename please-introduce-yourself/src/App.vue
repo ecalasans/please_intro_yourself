596b997f1a6287b8de7c248946f4631609d04850
ecalasans <e1906d0103>
2019-01-29 13:32:33 -0300

    Parei na pg 11

--- please-introduce-yourself/src/App.vue
+++ please-introduce-yourself/src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" class="jumbotron">
+    <div class="container">
+      <h1>Hello!  Nice to meet you!</h1>
+      <div v-for="message in messages" :key="message.id">
+        <h4>{{ message.title}}</h4>
+        <p>{{ message.text }}</p>
+        <p>{{ message.timestamp }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyBThlzNI-L4dky9SnUhy1LC_MzcVgzOqiU',
+  authDomain: 'pleaseintroduceyourself-f2906.firebaseapp.com',
+  databaseURL: 'https://pleaseintroduceyourself-f2906.firebaseio.com',
+  projectId: 'pleaseintroduceyourself-f2906',
+  storageBucket: 'pleaseintroduceyourself-f2906.appspot.com',
+  messagingSenderId: '474567875924'
+}
+let app = Firebase.initializeApp(config)
+
+let db = app.database()
+let messagesRef = db.ref('messages')
+
 export default {
-  name: 'App'
+  name: 'App',
+  firebase: {
+    messages: messagesRef
+  }
 }
 </script>
 
